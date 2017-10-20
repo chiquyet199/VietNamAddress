a66f102411bd5523f758799cffdd93f403e8c6fe
DESKTOP-KOIRU95\chiquyet199 <chiquyet199@gmail.com>
2017-10-20 17:41:16 +0700

    Fix fail eslint

--- src/index.js
+++ src/index.js
@@ -15,5 +15,6 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
   </Provider>
-  , document.querySelector('.container'));
-  
+  , document.querySelector('.container'),
+);
+
